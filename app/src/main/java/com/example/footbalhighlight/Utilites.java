1ab3e098bcaadd38f95d7023aed280d766ec6b17
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-23 19:17:42 +0300

    change of date format

--- app/src/main/java/com/example/footbalhighlight/Utilites.java
+++ app/src/main/java/com/example/footbalhighlight/Utilites.java
@@ -1,62 +1,36 @@
 package com.example.footbalhighlight;
 
 
-import android.util.Log;
+import android.os.Build;
 
-import java.sql.Timestamp;
+import androidx.annotation.RequiresApi;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class Utilites {
 
 
+    String mydate;
 
     public String dateFormater(String date) {
 
         String[] split = date.split("T");
-        String firstSubString = split[0].trim();
-//        // This is the format date we want
-//        DateFormat mSDF = new SimpleDateFormat("dd MMMM yyyy");
-//
-//        // This format date is actually present
-//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
-//        try {
-//            String mydate= mSDF.format(formatter.parse(firstSubString));
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
+        String firstSubString = split[0].toString();
+        // This is the format date we want
+        DateFormat mSDF = new SimpleDateFormat("dd MMMM yyyy");
 
-       return firstSubString;
-    }
-
-
-    public  String extractUrls(String text)
-    {
-
-        String theUrl = "";
-        List<String> containedUrls = new ArrayList<String>();
-        String urlRegex = "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
-        Pattern pattern = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE);
-        Matcher urlMatcher = pattern.matcher(text);
-
-        while (urlMatcher.find())
-        {
-            containedUrls.add(text.substring(urlMatcher.start(0),
-                    urlMatcher.end(0)));
+        // This format date is actually present
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+        try {
+            mydate= mSDF.format(formatter.parse(firstSubString));
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
-        for (String url : containedUrls)
-        {
-            theUrl = url;
-        }
-
-        return theUrl;
+       return mydate;
     }
+
 }
